2bb5c9d870a4eb7351d70a49fc65987ce2d6f553
Alejandro Mejia <aalejandro.mreyes@icloud.com>
2019-08-27 21:18:52 -0400

    Adds logger and validateUserId custom middleware, buggy

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
-const express = 'express';
+const express = require("express");
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+const router = require("./users/userRouter");
+
+server.use(express.json());
+
+server.use(logger);
+
+server.use("/api/users", router);
+
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
 function logger(req, res, next) {
-
-};
+  console.log(
+    `METHOD: ${req.method} => URL: ${
+      req.url
+    } => TIME: ${new Date().toISOString()}`
+  );
+  next();
+}
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,42 @@
-const express = 'express';
+const express = require("express");
+
+const db = require("./userDb");
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+function validateUserId(req, res, next) {
+  const user = db.getById(req.params.id);
+  if (id && user) {
+    req.user = user;
+    return next();
+  }
+  res.status(400).json({
+    message: "invalid user id"
+  });
+}
 
+router.post("/", (req, res) => {});
+
+router.post("/:id/posts", validateUserId, (req, res) => {});
+
+router.get("/", (req, res) => {});
+
+router.get("/:id", validateUserId, (req, res) => {
+  res.status(200).json(req.user);
 });
 
-router.post('/:id/posts', (req, res) => {
+router.get("/:id/posts", (req, res) => {});
 
-});
+router.delete("/:id", (req, res) => {});
 
-router.get('/', (req, res) => {
-
-});
-
-router.get('/:id', (req, res) => {
-
-});
-
-router.get('/:id/posts', (req, res) => {
-
-});
-
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
-
-});
+router.put("/:id", (req, res) => {});
 
 //custom middleware
 
-function validateUserId(req, res, next) {
+function validateUserId(req, res, next) {}
 
-};
+function validateUser(req, res, next) {}
 
-function validateUser(req, res, next) {
-
-};
-
-function validatePost(req, res, next) {
-
-};
+function validatePost(req, res, next) {}
 
 module.exports = router;
